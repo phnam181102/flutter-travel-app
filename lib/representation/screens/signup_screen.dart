9ebc8cd8ba0f9cd334f56da0e910bdc73cc31a57
hanh54747 <hanh54747@donga.edu.vn>
2022-12-05 16:48:35 +0700

    Make Login - Sign Up - Forgot Password - Splash Screen

---
+++ lib/representation/screens/signup_screen.dart
@@ -0,0 +1,297 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController nameController = TextEditingController();
+
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
+
+  bool _passwordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 140, 0, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(left: 113, right: 113),
+                      child: SizedBox(
+                        width: 149,
+                        height: 34,
+                        child: Center(
+                          child: Text(
+                            'Sign up now',
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      )),
+                  Padding(
+                    padding: EdgeInsets.only(left: 45, top: 12, right: 45),
+                    child: SizedBox(
+                      width: 285,
+                      height: 20,
+                      child: Center(
+                        child: Text(
+                          'Please fill the details and create account',
+                          style: TextStyle(
+                            color: Color.fromRGBO(125, 132, 141, 1),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20, top: 40, right: 20),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          Text(
+                            '',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black12,
+                            ),
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.black.withOpacity(0.075),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(14),
+                                  borderSide: BorderSide.none,
+                                )),
+                            controller: nameController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return ("Nhập tên của bạn");
+                              }
+                            },
+                          ),
+                          SizedBox(
+                            height: 24,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.black.withOpacity(0.075),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(14),
+                                  borderSide: BorderSide.none,
+                                )),
+                            controller: emailController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return ("Vui lòng nhập Email");
+                              }
+                              if (!RegExp(
+                                      "^[a-zA-Z0-9+_.-]+@[a-z0-9A-Z.-]+.[a-z]")
+                                  .hasMatch(value)) {
+                                return ("Vui lòng nhập Email hợp lệ");
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              emailController.text = value!;
+                            },
+                          ),
+                          Text(
+                            '',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    // Based on passwordVisible state choose the icon
+                                    _passwordVisible
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                    color: Theme.of(context).primaryColorDark,
+                                  ),
+                                  onPressed: () {
+                                    // Update the state i.e. toogle the state of passwordVisible variable
+                                    setState(() {
+                                      _passwordVisible = !_passwordVisible;
+                                    });
+                                  },
+                                ),
+                                filled: true,
+                                fillColor: Colors.black.withOpacity(0.075),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(14),
+                                  borderSide: BorderSide.none,
+                                )),
+                            controller: passwordController,
+                            keyboardType: TextInputType.text,
+
+                            validator: (value) {
+                              RegExp regex = RegExp(r'^.{6,}$');
+                              if (value!.isEmpty) {
+                                return ("Mật khẩu bắt buộc để đăng nhập");
+                              }
+                              if (!regex.hasMatch(value)) {
+                                return ("Mật khẩu không hợp lệ");
+                              }
+                            },
+                            onSaved: (value) {
+                              passwordController.text = value!;
+                            },
+                            obscureText: !_passwordVisible,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20, top: 16, right: 165),
+                    child: SizedBox(
+                        width: 190,
+                        height: 16,
+                        child: Center(
+                          child: Text(
+                            'Password must be 8 character',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Color.fromRGBO(125, 132, 141, 1),
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        )),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20, top: 40, right: 20),
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              print("Đăng nhập thành công");
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.blueAccent),
+                          child: SizedBox(
+                            width: 335,
+                            height: 56,
+                            child: Center(
+                              child: Text(
+                                'Sign In',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Don't have an account?",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color.fromRGBO(125, 132, 141, 1),
+                                )),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.blueAccent,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Or connect",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color.fromRGBO(125, 132, 141, 1),
+                                )),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 80,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'images/FB.png',
+                              width: 44,
+                              height: 44,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Image.asset(
+                              'images/IG.png',
+                              width: 44,
+                              height: 44,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Image.asset(
+                              'images/TW.png',
+                              width: 44,
+                              height: 44,
+                              fit: BoxFit.cover,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
